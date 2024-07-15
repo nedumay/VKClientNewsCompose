2452587ecb3a7d8c4efd46fd9e828fe965c5a039
Samim <Samim058@gamil.com>
2024-07-15 16:30:00 +0300

    PostCard VK, refactoring. Added TopAppBar

--- app/src/main/java/com/example/vknewclient/ui/postcard/PostCardVK.kt
+++ app/src/main/java/com/example/vknewclient/ui/postcard/PostCardVK.kt
@@ -1,20 +1,24 @@
 package com.example.vknewclient.ui.postcard
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material.icons.rounded.Send
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,143 +32,172 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.vknewclient.R
+import com.example.vknewclient.ui.theme.VKNewClientTheme
 
-@Preview(showBackground = true)
 @Composable
 fun PostCardVK() {
     Card(
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.background
+            containerColor = MaterialTheme.colorScheme.primary
         ),
-        border = BorderStroke(1.dp, Color.LightGray),
         elevation = CardDefaults.cardElevation(
-            defaultElevation = 10.dp
+            defaultElevation = 16.dp
         ),
         modifier = Modifier
-            .padding(4.dp)
             .fillMaxWidth()
-            .height(600.dp)
+            .padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
+            HeaderPost()
+            Spacer(modifier = Modifier.height(8.dp))
+            ImagePost()
+            Spacer(modifier = Modifier.height(8.dp))
+            BottomPost()
+        }
+    }
+}
 
+@Composable
+private fun HeaderPost(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 1.dp,
+                    color = Color.LightGray,
+                    shape = CircleShape
+                ),
+            painter = painterResource(id = R.drawable.avatar),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column (
+            modifier = Modifier
+                .weight(1f),
+            verticalArrangement = Arrangement.Center,
+        ){
+            Text(
+                text = "Name",
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "18:30",
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = "",
+            tint = MaterialTheme.colorScheme.onPrimary
+
+        )
+    }
+}
+
+@Composable
+private fun ImagePost(){
+    Text(
+        text = "Здесь название поста, очень длинное название",
+        modifier = Modifier
+            .fillMaxWidth()
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+    Image(
+        painter = painterResource(id = R.drawable.picture_post),
+        contentDescription = "",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(350.dp),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+private fun BottomPost(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround
 
     ) {
         Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-
+                .weight(1f)
         ) {
-            Image(
-                modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape)
-                    .border(
-                        width = 1.dp,
-                        color = Color.LightGray,
-                        shape = CircleShape
-                    ),
-                painter = painterResource(id = R.drawable.avatar),
-                contentDescription = "",
-                contentScale = ContentScale.Crop,
+            Text(
+                text = "255",
             )
-            Column (
-                modifier = Modifier
-                    .padding(start = 8.dp)
-                    .height(50.dp)
-                    .weight(2f),
-                verticalArrangement = Arrangement.Center,
-            ){
-                Text(
-                    text = "Name",
-                    textAlign = TextAlign.Center,
-                    color = MaterialTheme.colorScheme.onSurface
-                )
-                Text(
-                    text = "18:30",
-                    textAlign = TextAlign.Center,
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
-            Image(
-                painter = painterResource(id = R.drawable.ic_more_vert),
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.ic_visibility),
                 contentDescription = "",
-
+                tint = MaterialTheme.colorScheme.onPrimary
             )
         }
-        Text(
-            text = "Здесь название поста, очень длинное название",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.picture_post),
-            contentDescription = "",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(350.dp)
-                .padding(8.dp),
-            contentScale = ContentScale.Crop
-        )
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceAround
-
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .weight(3f)
-            ){
-                Text(
-                    text = "255",
-                )
-                Image (
-                    modifier = Modifier
-                        .padding(start = 4.dp),
-                    painter = painterResource(id = R.drawable.ic_visibility),
-                    contentDescription = ""
-                )
-            }
-            Row(verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .weight(1f),
-                horizontalArrangement = Arrangement.SpaceAround
-            ){
-                Text(
-                    text = "267",
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(end = 8.dp)
-                )
-                Image (
-                    painter = painterResource(id = R.drawable.ic_visibility),
-                    contentDescription = ""
-                )
-                Text(
-                    text = "13",
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(end = 8.dp)
-                )
-                Image (
-                    painter = painterResource(id = R.drawable.ic_visibility),
-                    contentDescription = ""
-                )
-                Text(
-                    text = "459",
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(end = 8.dp)
-                )
-                Image (
-                    painter = painterResource(id = R.drawable.ic_visibility),
-                    contentDescription = ""
-                )
-            }
+            Text(
+                text = "267",
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                imageVector = Icons.Rounded.Send,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = "13",
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.chat_bubble_outline),
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = "459",
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                imageVector = Icons.Rounded.FavoriteBorder,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
         }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLight(){
+    VKNewClientTheme(darkTheme = false) {
+        PostCardVK()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDark(){
+    VKNewClientTheme(darkTheme = true) {
+        PostCardVK()
     }
 }
--- app/src/main/java/com/example/vknewclient/ui/theme/Theme.kt
+++ app/src/main/java/com/example/vknewclient/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.vknewclient.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -13,29 +12,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    background = Color.Black,
-    surface = Color.Black,
+    primary = Color.Black,
+    secondary = Color.Black,
+    tertiary = Color.Black,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color.White,
-    onSurface = Color.White,
+    onSecondary = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
+    primary = Color.White,
+    secondary = Color.White,
+    tertiary = Color.White,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black
+
 
 )
 
