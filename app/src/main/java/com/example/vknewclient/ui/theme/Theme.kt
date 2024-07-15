1d9cfd6de99aa141876f73b16d34f184d15f5d18
Samim <Samim058@gamil.com>
2024-07-15 16:30:00 +0300

    PostCard VK, refactoring. Added TopAppBar

--- app/src/main/java/com/example/vknewclient/ui/theme/Theme.kt
+++ app/src/main/java/com/example/vknewclient/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.vknewclient.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,25 +9,28 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color.Black,
-    secondary = Color.Black,
-    tertiary = Color.Black,
-    onPrimary = Color.White,
-    onSecondary = Color.White
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color.White,
-    secondary = Color.White,
-    tertiary = Color.White,
-    onPrimary = Color.Black,
-    onSecondary = Color.Black
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
-
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
 )
 
 @Composable
